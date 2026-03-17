b7a41072c1f44634d783a17b81a1989c52b8ea20
aziz-boujaada <azizboujaada87@gmail.com>
2026-03-17 23:25:10 +0000

    feat: implement login functionality with JWT authentication and add LoginRequest validation

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,19 +3,27 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use App\Models\Villager;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display register and login forms .
      */
-    public function index()
+    public function registerForm()
     {
         return view('Auth.register');
+    }
+
+    public function loginForm()
+    {
+        return view('Auth.login');
     }
 
     /**
@@ -60,9 +68,26 @@ class AuthController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function login(LoginRequest $request)
     {
-        //
+        $data = $request->only('email' , 'password');
+       if(!Auth::attempt($data)){
+          return response()->json([
+            'status' => 'failed',
+            'message' => 'invalid email or password'
+          ],401);
+       }
+
+       $user = User::where('email' , $data['email'])->first();
+       $token = JWTAuth::fromUser($user);
+
+       return response()->json([
+          'status' => 'success',
+          'message' => 'You are Logged in with success',
+          'token' => $token
+       ],200)->cookie('token' ,$token , 2880 ,null , null , false , true );
+
+       
     }
 
     /**
