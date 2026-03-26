694d32c21e6cb0eb7f6675c103403b6b943c3267
aziz-boujaada <azizboujaada87@gmail.com>
2026-03-26 22:03:53 +0100

    refactor: improve StoreReadingService methods for better readability and validation

--- app/Services/StoreReadingService.php
+++ app/Services/StoreReadingService.php
@@ -1,47 +1,75 @@
 <?php
+
 namespace App\Services;
 
 use App\Models\MeterReadings;
+use Carbon\Carbon;
 use Exception;
 
-class StoreReadingService {
+class StoreReadingService
+{
 
-   public static function storeReading($reading_data){
-     
 
-     $consumption = 0 ; 
+   // give the last current reading to set as previous to register a new reading 
+   public static function  getPreviousReading($meter_id)
+   {
 
-     if($reading_data['previous_reading'] > $reading_data['current_reading']){
-        throw new \Exception('cuerrnt reading must be grater than previous reading ');
-     }else{
-         $consumption = $reading_data['current_reading'] - $reading_data['previous_reading'];
-     }
-     
-    $monthHasRecord = MeterReadings::where('meter_id' , $reading_data['meter_id'])
-    ->whereDate('reading_date' , $reading_data['reading_date'])
-    ->exists();
+      $previous_reading = MeterReadings::where('meter_id', $meter_id)
+         ->latest('reading_date')
+         ->first();
 
-    if($monthHasRecord){
-          throw new \Exception('You already add readings of this month');
-    }
+      return  $previous_reading->current_reading ?? 0;
+   }
 
-    $lastReading = MeterReadings::where('meter_id' , $reading_data['meter_id'])
-    ->latest('reading_date')
-    ->first() ;
+   // calculate cinsumption and check if urrent reading greater than previous  befor calcul
 
-    if($lastReading && $reading_data['previous_reading'] < $reading_data['current_reading']){
-                  throw new \Exception('previous reading must be mutch to current');
+   public static function calculateConsumpation($reading_data, $previous_reading)
+   {
+      $consumption = 0;
 
-    }
-     
-     $reading = MeterReadings::create([
+      if ($previous_reading > $reading_data['current_reading']) {
+         throw new \Exception('cuerrnt reading must be grater than previous reading ');
+      } else {
+         $consumption = $reading_data['current_reading'] - $previous_reading;
+      }
+      return $consumption;
+   }
+
+   // check if raeding not suplicate at same month 
+   public static function checkDuplicateReading($reading_data):void
+   {
+
+      $date = Carbon::parse($reading_data['reading_date']);
+      $monthHasRecord = MeterReadings::where('meter_id', $reading_data['meter_id'])
+         ->whereMonth('reading_date', $date->month)
+         ->whereYear('reading_date', $date->year)
+         ->exists();
+
+      // if ($date->month < Carbon::now()->month) {
+      //    throw new \Exception('You cant set a old date  ');
+      // }
+      
+            if ($monthHasRecord) {
+               throw new \Exception('You already add readings of this month');
+            }
+   }
+
+   public static function storeReading($reading_data)
+   {
+      self::checkDuplicateReading($reading_data);
+      $previous_reading  = self::getPreviousReading($reading_data['meter_id']);
+      $consumption = self::calculateConsumpation($reading_data, $previous_reading);
+
+
+
+      $reading = MeterReadings::create([
          'meter_id' => $reading_data['meter_id'],
-         'previous_reading' => $reading_data['previous_reading'],
+         'previous_reading' => $previous_reading,
          'current_reading' => $reading_data['current_reading'],
          'consumption' => $consumption,
          'reading_date' => $reading_data['reading_date']
-         ]);
-         
-         return $reading;
+      ]);
+
+      return $reading;
    }
 }
