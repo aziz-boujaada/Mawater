a235ba0322c5ee1d9be4d999ab4d4f59b7e62a6f
aziz-boujaada <azizboujaada87@gmail.com>
2026-04-03 22:49:28 +0100

    fix: correct role name in RepairAgentMiddleware and update route middleware alias

--- app/Http/Middleware/RepairAgentMiddleware.php
+++ app/Http/Middleware/RepairAgentMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class RepairAgentMiddleware
@@ -15,8 +16,8 @@ class RepairAgentMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $user =  $request->user();
-        if ($user?->role != 'reapair_agent') {
+        $user =  Auth::user();
+        if($user->role != 'repair_agent') {
             return redirect()->route('home');
         }
 
