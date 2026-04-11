a2140905801c58e3b999ebba99e8442f1db9dd5c
aziz-boujaada <azizboujaada87@gmail.com>
2026-04-11 22:28:58 +0100

    feat: implement user profile update functionality with validation rules

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,13 +1,41 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateProfileRequest;
 use App\Models\User;
+use App\Services\UpdateProfileService;
 
-class UserController {
-    public function index(){
+class UserController
+{
+    public function index()
+    {
 
-       $users = User::with('villager')->paginate(10);
-       return view('dashboards.users.index' , compact('users'));
+        $users = User::with('villager')->paginate(10);
+        return view('dashboards.users.index', compact('users'));
+    }
+
+
+    public function showUser(int $id)
+    {
+        $user = User::with('villager')->where('id', $id)->first();
+        return view('dashboards.users.show', compact('user'));
+    }
+
+    public function editUser(int $id)
+    {
+        $user = User::findOrFail($id);
+        return view('dashboards.users.edit', compact('user'));
+    }
+
+    public function updateProfile(UpdateProfileRequest $requet, UpdateProfileService $updateProfileService , int $id)
+    {
+
+        $profile_data = $requet->validated();
+
+        $user = User::findOrFail($id) ; 
+        $updateProfileService->updateProfile($profile_data , $user);
+
+        return redirect()->route('users')->with('success' , 'user updated successfully');
     }
 }
---
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+      public function rules(): array
+{
+    return [
+        'name' => 'required|string|max:255',
+        'email' => 'required|email',
+        'role' => 'required|in:admin,collector,repair_agent,villager',
+        'phone' => 'required|string|max:15',
+        'cin' => 'required_if:role,villager|string|max:20',
+        'subscription_status' => 'required_if:role,villager|in:subscribed,not_subscribed',
+        'address' => 'required_if:role,villager|string|max:255',
+    ];
+}
+
+    public function messages(): array
+    {
+        return [
+            'cin.required_if' => 'CIN is required for Villager role',
+            'address.required_if' => 'Address is required for Villager role',
+        ];
+    }
+}
